582aeebe3a35c2c321ad952f2119f29cbca1e698
douglasvsf <douglas_vsf@hotmail.com>
2023-10-18 22:46:06 -0300

    feat: novo projeto typescript

---
+++ alura-studies/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./components/form";
+import ListData from "./components/List";
+
+function App() {
+  return (
+    <div>
+      <Form />
+      <ListData />
+    </div>
+  );
+}
+
+export default App;
---
+++ alura-studies/src/components/button/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Button extends React.Component {
+  render(): React.ReactNode {
+    return <button className="bg-slate-500 rounded-sm">botão</button>;
+  }
+}
+
+export default Button;
